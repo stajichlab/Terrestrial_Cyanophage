4e924bec385b825e981a3e8ac65b29268939b723
Cassie <clettinger@ucdavis.edu>
2023-03-20 16:29:27 -0700

    Adding cyano-vOTU annotation

---
+++ scripts/11_votu_annotation.sh
@@ -0,0 +1,5 @@
+#running cenote-taker2 interactively
+
+conda activate cenote-taker2_env
+
+run_cenote-taker2.py -c cyanophage/cyano.votu.fa -r viruses_am_ct -m 8 -t 8 -p False -am True -hh hhsearch
